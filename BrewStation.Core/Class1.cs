d74883318dc2f2186ba9ebdb3703fa76191cccf8
Solomon <s.manalo@bettechnology.com>
2026-03-14 00:07:10 +0800

    Squashed

--- BrewStation.Core/Class1.cs
+++ BrewStation.Core/Class1.cs
@@ -2,6 +2,18 @@
 {
     public class Class1
     {
+      public int AddTwoNumbers(int a, int b) => a + b;
+      public int SubractTwoNumbers(int a, int b) => a - b;
+      public int MultiplyTwoNumbers(int a, int b) => a * b;
+      public int DivideTwoNumbers(int a, int b) => a / b;
+    }
+
+    public class MasterChanges
+    {
+        public int Add(int a, int b) => a+b;
+        public int Subtract(int a, int b) => a-b;
+        public int Multiply(int a, int b) => a*b;
+        public int Divide(int a, int b) => a/b;
 
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # BrewStation
 
+Dev1 Change4
 Dev1 Change3
 Dev1 Change2
 Master Change 2
 Master Change 1
 Dev1 Change1
+
+Dev1 Commit1
+Dev1 Commit2
+Dev1 Commit3
